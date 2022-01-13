6bbf176e857440147a973e10af71b5b87189b82a
Surya <123004250@sastra.ac.in>
2022-01-13 18:39:39 +0530

    refactor: updated to es6+

--- app.js
+++ app.js
@@ -1,50 +1,49 @@
-var dob=document.querySelector("#dob");
-var luckNumber=document.querySelector("#lucky-no");
-var checkButton=document.querySelector("#check-btn");
-var container=document.querySelector(".container")
-var message=document.createElement("p");
-var privMsg=document.querySelector("#privacy");
-var closeBtn=document.querySelector("#close");
-console.log(privMsg,closeBtn);
+let dob = document.querySelector("#dob");
+let luckNumber = document.querySelector("#lucky-no");
+let checkButton = document.querySelector("#check-btn");
+let container = document.querySelector(".container")
+let message = document.createElement("p");
+let privMsg = document.querySelector("#privacy");
+let closeBtn = document.querySelector("#close");
+console.log(privMsg, closeBtn);
 
-function sumOfDigits(doB){
-    var dob=Number(doB);
-    
-    var sum=0;
-    while(dob>0){
-       sum=sum+dob % 10;
-       dob=Math.trunc(dob/10);
+const sumOfDigits = doB => {
+    let dob = Number(doB);
+
+    let sum = 0;
+    while (dob > 0) {
+        sum = sum + dob % 10;
+        dob = Math.trunc(dob / 10);
     }
-    
+
     return sum;
 }
-function checkLukyNumber(dob,luckNumber){
-    var doB=dob;
+const checkLukyNumber = (dob, luckNumber) => {
+    let doB = dob;
 
-    doB=doB.replaceAll("-","");
-    var sum=sumOfDigits(doB);
-    if(sum%luckNumber===0)
-    {   message.remove();
-        message.innerText="You are Lucky👏"
+    doB = doB.replaceAll("-", "");
+    let sum = sumOfDigits(doB);
+    if (sum % luckNumber === 0) {
+        message.remove();
+        message.innerText = "You are Lucky👏"
         container.appendChild(message);
-    }else{
+    } else {
         message.remove()
-        message.innerText="Go Work Until you Become Lucky"
+        message.innerText = "Go Work Until you Become Lucky"
         container.appendChild(message);
     }
 
 }
-function checkHandler(){
-   if(dob.value===""||luckNumber.value===""){
-   message.innerText="Enter a valid date or luky number";
-    container.appendChild(message);
-   }
-   else{
-       message.remove();
-       checkLukyNumber(dob.value,luckNumber.value);
-   }
+const checkHandler = () => {
+    if (dob.value === "" || luckNumber.value === "") {
+        message.innerText = "Enter a valid date or luky number";
+        container.appendChild(message);
+    } else {
+        message.remove();
+        checkLukyNumber(dob.value, luckNumber.value);
+    }
 }
-checkButton.addEventListener("click",checkHandler);
-closeBtn.addEventListener("click",function(){
-  privMsg.style.display="none";
+checkButton.addEventListener("click", checkHandler);
+closeBtn.addEventListener("click", function () {
+    privMsg.style.display = "none";
 });
